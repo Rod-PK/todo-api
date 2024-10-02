0f920086ae289c83d359dff91cf23fafed3b3c49
Rodney Hinson <rodneyhinson22@gmail.com>
2024-10-02 09:03:18 +0000

    changes to js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ import todoRouter from "./routes/todo.js";
 // Create an express app
 const app = express();
 
-// Define routes
+// Use routes
 // app.get("/hello", (req, res, next) => {
 //     // console.log(req.headers);
 //     res.json("You visited the hello endpoint!");
